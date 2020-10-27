f468a68c58d57c9c94951a2ddcae01f329d6726a
IlyaMotroshilov <70964678+IlyaMotroshilov@users.noreply.github.com>
2020-10-27 20:51:57 +0300

    Update Сложение и умножение массивов.py
    Всё работает как часы

--- Сложение и умножение массивов.py
+++ Сложение и умножение массивов.py
@@ -15,22 +15,21 @@ def matrix(a):
         for el in arr:
             print(el, end = ' ')
         print()
+        
 matrix(a)
+
 print('Введите матрицу B: ')
 for i in range(0, 3):
     for j in range(0, 3):
         b[i][j] = int(input("Ввод значения: "))
-def matrix(b):
-    for arr in b:
-        for el in arr:
-            print(el, end = ' ')
-        print()
+        
 matrix(b)
+
 print("Сумма матриц равна: ")
-print(matrix(a)+matrix(b))
+matrix(a+b)
 
 print("Произведение матриц равно: ")
-print(matrix(a)*matrix(b))
+matrix(a*b)
 
 
 
